11f99256d6b1b735b00d3adc8eee272362311848
Lincoln-Ab <abramodipietro@libero.it>
2023-07-07 22:51:26 +0200

    fix :  animator

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,42 +9,56 @@ public class PlayerMovement : MonoBehaviour
 
     private float movingStep = 1f;
 
-    void Start()
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Start()
     {
         nextPosition = gameObject.transform.position;
     }
 
-    void Update()
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow) & !isMoving)
         {
             isMoving = true;
             nextPosition += Vector2.up * movingStep;
+            anim.SetInteger("direction", 12);
         }
 
         if (Input.GetKeyDown(KeyCode.DownArrow) & !isMoving)
         {
             isMoving = true;
             nextPosition += Vector2.down * movingStep;
+            anim.SetInteger("direction", 6);
         }
 
         if (Input.GetKeyDown(KeyCode.LeftArrow) & !isMoving)
         {
             isMoving = true;
             nextPosition += Vector2.left * movingStep;
+            anim.SetInteger("direction", 9);
         }
 
         if (Input.GetKeyDown(KeyCode.RightArrow) & !isMoving)
         {
             isMoving = true;
             nextPosition += Vector2.right * movingStep;
+            anim.SetInteger("direction", 3);
         }
 
         transform.position = Vector2.Lerp(transform.position, nextPosition, velocity * Time.deltaTime);
         if (Vector2.Distance(transform.position, nextPosition) < 0.01f)
         {
             transform.position = nextPosition;
+            anim.SetInteger("direction", 0);
             isMoving = false;
         }
     }
+
+
 }

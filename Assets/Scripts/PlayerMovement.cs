50d38697f4b131ce36745ebffaf7d5975d92ab77
Simone Lungarella <simonelungarella@gmail.com>
2023-07-08 09:06:21 +0200

    fix: fixed cherry jump animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float velocity = 20f;
+    private float velocity = 10f;
     private bool isMoving = false;
 
     private Vector2 nextPosition;
@@ -34,40 +34,41 @@ public class PlayerMovement : MonoBehaviour
         {
             isMoving = true;
             nextPosition += Vector2.up * movingStep;
-            anim.SetInteger("direction", 12);
+            anim.SetBool("isJumping", isMoving);
         }
 
         if (Input.GetKeyDown(KeyCode.DownArrow) & !isMoving)
         {
             isMoving = true;
             nextPosition += Vector2.down * movingStep;
-            anim.SetInteger("direction", 6);
+            anim.SetBool("isJumping", isMoving);
         }
 
         if (Input.GetKeyDown(KeyCode.LeftArrow) & !isMoving)
         {
             isMoving = true;
             nextPosition += Vector2.left * movingStep;
-            anim.SetInteger("direction", 9);
+            anim.SetBool("isJumping", isMoving);
         }
 
         if (Input.GetKeyDown(KeyCode.RightArrow) & !isMoving)
         {
             isMoving = true;
             nextPosition += Vector2.right * movingStep;
-            anim.SetInteger("direction", 3);
+            anim.SetBool("isJumping", isMoving);
         }
 
         // lerp della posizione 
         transform.position = Vector2.Lerp(transform.position, nextPosition, velocity * Time.deltaTime);
-      
+
         // arrotondamento della posizione e fine movimento
-        if (Vector2.Distance(transform.position, nextPosition) < 0.01f)
+        if (Vector2.Distance(transform.position, nextPosition) < 0.1f)
         {
-            transform.position = nextPosition;
-            anim.SetInteger("direction", 0);
             isMoving = false;
+            transform.position = nextPosition;
+            anim.SetBool("isJumping", isMoving);
         }
+
     }
 
 
